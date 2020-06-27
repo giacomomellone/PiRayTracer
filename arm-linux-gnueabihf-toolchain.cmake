ca998eec4dcf46d02f6b792066f476f16514da99
Giacomo <giacomo.mellone@gmail.com>
2020-06-27 18:25:40 +0200

    Added toolchain for raspberry Pi, modified Eclipse build targets

---
+++ arm-linux-gnueabihf-toolchain.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_CROSSCOMPILING TRUE)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set(OBJCOPY arm-linux-gnueabihf-objcopy)
