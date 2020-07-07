5609b367dd3b8a85199899f1b5f38cdd89bd7f7c
Giacomo <giacomo.mellone@gmail.com>
2020-07-08 00:03:19 +0200

    Added first chapter test (projectile)

---
+++ tests/chap1Tester.cpp
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ *    INCLUDED FILES
+ ******************************************************************************/
+#include <CppUTest/TestHarness.h>
+
+//-- module being tested
+#include "../include/tuple.h"
+
+
+/*******************************************************************************
+ *    DEFINITIONS
+ ******************************************************************************/
+
+/*******************************************************************************
+ *    PRIVATE TYPES
+ ******************************************************************************/
+class Projectile
+{
+    public:
+        Tuple position, velocity;
+
+        Projectile(Tuple position, Tuple velocity);
+};
+
+class Environment
+{
+    public:
+        Tuple gravity, wind;
+
+        Environment(Tuple gravity, Tuple wind);
+};
+
+Projectile tick(Environment e, Projectile p);
+
+/*******************************************************************************
+ *    PRIVATE DATA
+ ******************************************************************************/
+
+/*******************************************************************************
+ *    PRIVATE FUNCTIONS
+ ******************************************************************************/
+Projectile::Projectile(Tuple position, Tuple velocity)
+{
+    this->position = position;
+    this->velocity = velocity;
+}
+
+Environment::Environment(Tuple gravity, Tuple wind)
+{
+    this->gravity = gravity;
+    this->wind = wind;
+}
+
+Projectile tick(Environment e, Projectile p)
+{
+    Tuple pos = p.position + p.velocity;
+    Tuple vel = p.velocity + e.gravity + e.wind;
+
+    return Projectile(pos, vel);
+}
+
+/*******************************************************************************
+ *    SETUP, TEARDOWN
+ ******************************************************************************/
+
+/*******************************************************************************
+ *    TESTS
+ ******************************************************************************/
+
+TEST_GROUP(Chap_1_Test) {};
+
+TEST(Chap_1_Test, projectile)
+{
+    Tuple projPosition = Point(0, 1, 0.3);
+    Tuple projVelocity = Vector(1, 1, -0.3);
+
+    Tuple envGravity = Vector(0, -0.05, 0);
+    Tuple envWind = Vector(-0.01, 0, 0);
+
+    Projectile p = Projectile(projPosition, projVelocity.Normalize());
+    Environment e = Environment(envGravity, envWind);
+
+    int count = 0;
+
+    while (p.position.y > 0)
+    {
+//        printf("\n%d: Proj.pos.x = %f, Proj.pos.y = %f, Proj.pos.z = %f",
+//                count, p.position.x, p.position.y, p.position.z);
+        p = tick(e, p);
+
+        count++;
+    }
+}
+
