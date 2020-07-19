5e0145e2825b9dc3b0b74bcbafe7e8464e156591
Giacomo <giacomo.mellone@gmail.com>
2020-07-20 00:14:40 +0200

    Updated mainTester cpps

--- tests/mainTester.cpp
+++ tests/mainTester.cpp
@@ -1,9 +1,14 @@
 #include <CppUTest/CommandLineTestRunner.h>
+
 #include "support/utils.cpp"
 #include "modules/tuple.cpp"
 #include "modules/canvas.cpp"
 #include "modules/matrix.cpp"
 #include "modules/transformation.cpp"
+#include "modules/ray.cpp"
+#include "modules/shapes/sphere.cpp"
+#include "modules/shapes/shape.cpp"
+#include "modules/intersection.cpp"
 
 int main(int argc, char **argv) {
     return CommandLineTestRunner::RunAllTests(argc, argv);
