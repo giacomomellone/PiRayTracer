7b41d087e216e048e63b9dad44feb58149ff2a3c
Giacomo <giacomo.mellone@gmail.com>
2020-09-05 11:20:08 +0200

    Modified makefile in order to compile for raspberry. Output generated

--- tests/chapEndTester.cpp
+++ tests/chapEndTester.cpp
@@ -197,57 +197,57 @@ TEST(EndChapterTest, Chap5_RaySphereIntersection)
 
 TEST(EndChapterTest, Chap6__LightAndShading)
 {
-//    uint32_t cv_px = 100;
-//    int wall_z = 10;
-//    float wall_size = 7.0;
-//    float pixel_size = wall_size / cv_px;
-//    float half = wall_size / 2;
-//    float world_x, world_y;
-//
-//    Sphere s;
-//    s.material.color = Color(1, 0, 0);
-//    s.material.ambient = 0.1;
-//    s.material.diffuse = 0.9;
-//    s.material.specular = 0.9;
-//    s.material.shininess = 200;
-//
-////    s.SetTranformation(Translation(1, -1, 0));
-//    Tuple rayOrigin = Point(0, 0, -5);
-//
-//    Canvas cv = Canvas(cv_px, cv_px);
-//
-//    Light lightP(Point(-10, 10, -10), Color(1, 1, 1));
-//
-//    for (uint32_t y = 0; y < cv_px - 1; y++)
-//    {
-//        world_y = half - pixel_size * y;
-//
-//        for (uint32_t x = 0; x < cv_px - 1; x++)
-//        {
-//            world_x = - half + pixel_size * x;
-//
-//            Tuple pW = Point(world_x, world_y, wall_z);
-//
-//            /* Normalize the ray direction, otherwise the light
-//             * cast won't work
-//             */
-//            Ray rW(rayOrigin, (pW - rayOrigin).Normalize());
-//
-//            vector<Intersection> xs = Intersect(s, rW);
-//
-//            Intersection *hit = Hit(&xs);
-//
-//            if (hit != NULL)
-//            {
-//                /* Calculate the normal vector at the hit */
-//                Tuple pointHit = rW.Position(hit->t);
-//                Tuple normalAtHit = hit->s.Normal(pointHit);
-//                Tuple eyeV = -rW.directionV;
-//
-//                cv.WritePixel(x, y, Lighting(s.material, lightP, pointHit, eyeV, normalAtHit));
-//            }
-//        }
-//    }
-//
-//    cv.SavePPM("0x005.ppm");
+    uint32_t cv_px = 100;
+    int wall_z = 10;
+    float wall_size = 7.0;
+    float pixel_size = wall_size / cv_px;
+    float half = wall_size / 2;
+    float world_x, world_y;
+
+    Sphere s;
+    s.material.color = Color(1, 0, 0);
+    s.material.ambient = 0.1;
+    s.material.diffuse = 0.9;
+    s.material.specular = 0.9;
+    s.material.shininess = 200;
+
+//    s.SetTranformation(Translation(1, -1, 0));
+    Tuple rayOrigin = Point(0, 0, -5);
+
+    Canvas cv = Canvas(cv_px, cv_px);
+
+    Light lightP(Point(-10, 10, -10), Color(1, 1, 1));
+
+    for (uint32_t y = 0; y < cv_px - 1; y++)
+    {
+        world_y = half - pixel_size * y;
+
+        for (uint32_t x = 0; x < cv_px - 1; x++)
+        {
+            world_x = - half + pixel_size * x;
+
+            Tuple pW = Point(world_x, world_y, wall_z);
+
+            /* Normalize the ray direction, otherwise the light
+             * cast won't work
+             */
+            Ray rW(rayOrigin, (pW - rayOrigin).Normalize());
+
+            vector<Intersection> xs = Intersect(s, rW);
+
+            Intersection *hit = Hit(&xs);
+
+            if (hit != NULL)
+            {
+                /* Calculate the normal vector at the hit */
+                Tuple pointHit = rW.Position(hit->t);
+                Tuple normalAtHit = hit->s.Normal(pointHit);
+                Tuple eyeV = -rW.directionV;
+
+                cv.WritePixel(x, y, Lighting(s.material, lightP, pointHit, eyeV, normalAtHit));
+            }
+        }
+    }
+
+    cv.SavePPM("0x005.ppm");
 }
